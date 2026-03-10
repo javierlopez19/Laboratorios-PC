bf908b0a7e6e1eddc73209c66182e205a404a574
javierlopez19 <humberthojavier@gmail.com>
2026-03-09 22:23:34 -0600

    Create ejercicio 4.cs

---
+++ Laboratorios 7/ejercicio 4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int pinCorrecto = 1234;
+        int pin;
+        int intentos = 0;
+
+        do
+        {
+            Console.WriteLine("Ingrese el PIN:");
+            pin = int.Parse(Console.ReadLine());
+
+            intentos++;
+
+            if (pin == pinCorrecto)
+            {
+                Console.WriteLine("Acceso concedido");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("PIN incorrecto");
+            }
+
+        } while (intentos < 3);
+
+        if (pin != pinCorrecto && intentos == 3)
+        {
+            Console.WriteLine("Cuenta bloqueada");
+        }
+    }
+}
