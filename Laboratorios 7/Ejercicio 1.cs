120e98b858c41d853f0e3e268199aa9c67df330b
javierlopez19 <humberthojavier@gmail.com>
2026-03-09 22:01:14 -0600

    Create Ejercicio 1.cs

---
+++ Laboratorios 7/Ejercicio 1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = 0;
+      
+        while (N <= 0)
+        {
+            Console.WriteLine("¿Cuántos números desea sumar?");
+            N = int.Parse(Console.ReadLine());
+        }
+
+        int contador = 1;
+        double numero;
+        double suma = 0;
+
+        while (contador <= N)
+        {
+            Console.WriteLine("Ingrese el número " + contador + ":");
+            numero = double.Parse(Console.ReadLine());
+
+            suma += numero;
+            contador++;
+        }
+
+        double promedio = suma / N;
+
+        Console.WriteLine("La suma total es: " + suma);
+        Console.WriteLine("El promedio es: " + promedio);
+
+
+    }
+}
