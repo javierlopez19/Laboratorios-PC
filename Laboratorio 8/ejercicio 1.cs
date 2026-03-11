88916d51d7e1a02d4a000e5d29f426895534951f
javierlopez19 <humberthojavier@gmail.com>
2026-03-11 09:41:37 -0600

    Create ejercicio 1.cs

---
+++ Laboratorio 8/ejercicio 1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int contador = 1;
+        int aprobados = 0;
+        int reprobados = 0;
+        double nota;
+        double suma = 0;
+        double promedio;
+
+        do
+        {
+            Console.WriteLine("Ingrese su nota (1-100): ");
+            nota = Convert.ToDouble((Console.ReadLine()));
+
+            suma += nota;
+            if (nota >= 61)
+            {
+                Console.WriteLine("Aprobado");
+                aprobados++;
+            }
+            else
+            {
+                Console.WriteLine("Reprueba");
+                reprobados++;
+
+            }
+            contador++;
+        }
+        while
+        
+        (contador <= 10);
+        promedio = suma / 10;
+
+        Console.WriteLine("Promedio de la clase: " + promedio);
+        Console.WriteLine("Cantidad de aprobados: " + aprobados);
+        Console.WriteLine("Cantidad de reprobados: " + reprobados);
+    
+    
+
+
+
+
+
+
+
+
+        }
+}
