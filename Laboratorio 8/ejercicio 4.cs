7a03c73eeb9e6e4a90d498845571b885d4c838f1
javierlopez19 <humberthojavier@gmail.com>
2026-03-17 17:45:25 -0600

    Create ejercicio 4.cs

---
+++ Laboratorio 8/ejercicio 4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero = 0;
+        int cantidad = 0;
+        int positivos = 0;
+        int negativos = 0;
+        int suma = 0;
+
+        Console.Write("Ingresa un número (0 para terminar): ");
+        numero = int.Parse(Console.ReadLine());
+
+        while (numero != 0)
+        {
+            cantidad++;
+            suma = suma + numero;
+
+            if (numero > 0)
+                positivos++;
+            else if (numero < 0)
+                negativos++;
+
+            Console.Write("Ingresa un número (0 para terminar): ");
+            numero = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Números ingresados: " + cantidad);
+        Console.WriteLine("Números positivos: " + positivos);
+        Console.WriteLine("Números negativos: " + negativos);
+        Console.WriteLine("Suma total: " + suma);
+
+    }
+}
