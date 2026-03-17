7599748d6bea9adc5bb1779cbec063095256e84c
javierlopez19 <humberthojavier@gmail.com>
2026-03-17 17:04:39 -0600

    Update Program.cs

--- Laboratorio 8/Ejercicio 2/Ejercicio 2/Program.cs
+++ Laboratorio 8/Ejercicio 2/Ejercicio 2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+     
+        Console.Write("Ingresa un número entero: ");
+        int n = int.Parse(Console.ReadLine());
+
+      
+        int suma = 0;
+        int pares = 0;
+        int impares = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            suma = suma + i;       
+
+            if (i % 2 == 0)
+                pares++;           
+            else
+                impares++;         
+        }
+
+        Console.WriteLine("Suma del 1 al " + n + ": " + suma);
+        Console.WriteLine("Cantidad de números pares: " + pares);
+        Console.WriteLine("Cantidad de números impares: " + impares);
+    }
+}
