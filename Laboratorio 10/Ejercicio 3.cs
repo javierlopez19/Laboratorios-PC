4e6504ef5d28785651185509804ff5e9cdbd848b
javierlopez19 <humberthojavier@gmail.com>
2026-04-14 21:09:38 -0600

    Create Ejercicio 3.cs

---
+++ Laboratorio 10/Ejercicio 3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    
+    static decimal AplicarDescuento(decimal porcentaje, ref decimal precio)
+    {
+
+        decimal montoDescontado = precio * porcentaje;
+
+        
+        precio = precio - montoDescontado;
+
+        
+        return montoDescontado;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el precio del producto: ");
+        decimal precio = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el porcentaje de descuento (ej. 0.20 para 20%): ");
+        decimal porcentaje = decimal.Parse(Console.ReadLine());
+
+      
+        decimal descuento = AplicarDescuento(porcentaje, ref precio);
+
+        Console.WriteLine("Monto descontado: " + descuento);
+        Console.WriteLine("Precio final con descuento: " + precio);
+    }
+}
