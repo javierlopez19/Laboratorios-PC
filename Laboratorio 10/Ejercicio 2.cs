c84c40e45d8931ab0e4ac0a421df7d866b878214
javierlopez19 <humberthojavier@gmail.com>
2026-04-14 20:54:35 -0600

    Create Ejercicio 2.cs

---
+++ Laboratorio 10/Ejercicio 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+
+    static string ElevarCuadrado(ref int numero)
+    {
+        numero = numero * numero;
+        return "La operación fue realizada correctamente.";
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número entero: ");
+        int n = int.Parse(Console.ReadLine());
+
+
+        string mensaje = ElevarCuadrado(ref n);
+
+        Console.WriteLine(mensaje);
+        Console.WriteLine("El número elevado al cuadrado es: " + n);
+    }
+}
