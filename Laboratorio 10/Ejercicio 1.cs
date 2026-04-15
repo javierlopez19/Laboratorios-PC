eb28784d617a3fd3f9906c2a30dbf9330d0659b0
javierlopez19 <humberthojavier@gmail.com>
2026-04-14 20:36:44 -0600

    Create Ejercicio 1.cs

---
+++ Laboratorio 10/Ejercicio 1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    
+    static int SumaDigitos(int numero)
+    {
+        int suma = 0;
+
+        while (numero > 0)
+        {
+            int digito = numero % 10;   
+            suma += digito;            
+            numero = numero / 10;       
+        }
+
+        return suma;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número entero positivo: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int resultado = SumaDigitos(n);
+
+        Console.WriteLine("La suma de los dígitos es: " + resultado);
+    }
+}
+
