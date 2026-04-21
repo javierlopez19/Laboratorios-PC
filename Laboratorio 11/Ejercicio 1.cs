6abd05e91aa683efdae4a041cc1c48b1d8f8c0a2
javierlopez19 <humberthojavier@gmail.com>
2026-04-21 17:49:50 -0600

    Create Ejercicio 1.cs

---
+++ Laboratorio 11/Ejercicio 1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese una contraseña: ");
+        string contra = Console.ReadLine();
+
+        string errores = ValidarContraseña(contra);
+
+        if (string.IsNullOrEmpty(errores))
+        {
+            Console.WriteLine("Contraseña válida");
+        }
+        else
+        {
+            Console.WriteLine("Inválida: " + errores);
+        }
+    }
+
+    static string ValidarContraseña(string pwd)
+    {
+        string errores = "";
+
+        
+        if (pwd.Length < 8)
+            errores += "falta longitud mínima; ";
+
+        
+        bool tieneMayuscula = false;
+        foreach (char c in pwd)
+        {
+            if (char.IsUpper(c))
+            {
+                tieneMayuscula = true;
+                break;
+            }
+        }
+        if (!tieneMayuscula)
+            errores += "falta mayúscula; ";
+
+        
+        bool tieneNumero = false;
+        foreach (char c in pwd)
+        {
+            if (char.IsDigit(c))
+            {
+                tieneNumero = true;
+                break;
+            }
+        }
+        if (!tieneNumero)
+            errores += "falta número; ";
+
+        
+        string especiales = "@#$%!¡?¿";
+        bool tieneEspecial = false;
+        foreach (char c in pwd)
+        {
+            if (especiales.Contains(c))
+            {
+                tieneEspecial = true;
+                break;
+            }
+        }
+        if (!tieneEspecial)
+            errores += "falta carácter especial; ";
+
+        return errores.Trim();
+    }
+}
