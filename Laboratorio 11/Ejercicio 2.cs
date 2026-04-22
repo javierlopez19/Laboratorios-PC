30e04955fdb12f613e580858e9c558559abdcff4
javierlopez19 <humberthojavier@gmail.com>
2026-04-21 18:17:30 -0600

    Create Ejercicio 2.cs

---
+++ Laboratorio 11/Ejercicio 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un texto: ");
+        string texto = Console.ReadLine();
+
+        string invertido = InvertirTexto(texto);
+
+        Console.WriteLine("Texto invertido: " + invertido);
+    }
+
+    static string InvertirTexto(string original)
+    {
+        string resultado = "";
+
+       
+        for (int i = original.Length - 1; i >= 0; i--)
+        {
+            resultado += original[i];
+        }
+
+        return resultado;
+    }
+}
