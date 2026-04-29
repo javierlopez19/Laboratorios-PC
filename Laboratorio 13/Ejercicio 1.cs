1d697b8557716ca1aaaf9d4a798914796bee0f6f
javierlopez19 <humberthojavier@gmail.com>
2026-04-29 09:56:45 -0600

    Create Ejercicio 1.cs

---
+++ Laboratorio 13/Ejercicio 1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Calculadora
+{
+    public double numero1;
+    public double numero2;
+
+    public double Sumar()
+    {
+        return numero1 + numero2;
+    }
+
+    public double Restar()
+    {
+        return numero1 - numero2;
+    }
+
+    public double Multiplicar()
+    {
+        return numero1 * numero2;
+    }
+
+    public double Dividir()
+    {
+        return numero1 / numero2;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculadora calc = new Calculadora();
+        calc.numero1 = 10;
+        calc.numero2 = 5;
+
+        Console.WriteLine("Suma: " + calc.Sumar());
