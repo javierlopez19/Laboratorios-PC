7e859733cbb97403368ee8036b3c2009a28ebf4e
javierlopez19 <humberthojavier@gmail.com>
2026-04-28 22:53:17 -0600

    Create Ejercicio 4.cs

---
+++ Laboratorio 12/Ejercicio 4.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matriz = new int[5, 5];
+
+       
+        Llenar(matriz);
+
+        
+        int sumaPrincipal = SumaDiagonalPrincipal(matriz);
+        int sumaSecundaria = SumaDiagonalSecundaria(matriz);
+
+        
+        Console.WriteLine("\nSuma de la diagonal principal: " + sumaPrincipal);
+        Console.WriteLine("Suma de la diagonal secundaria: " + sumaSecundaria);
+    }
+
+   
+    static void Llenar(int[,] m)
+    {
+        Console.WriteLine("Ingrese los valores de la matriz 5x5:");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write("Elemento [" + i + "][" + j + "]: ");
+                m[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+
+    
+    static int SumaDiagonalPrincipal(int[,] m)
+    {
+        int suma = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            suma += m[i, i]; 
+        }
+        return suma;
+    }
+
+    
+    static int SumaDiagonalSecundaria(int[,] m)
+    {
+        int suma = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            suma += m[i, 4 - i]; 
+        }
+        return suma;
+    }
+}
