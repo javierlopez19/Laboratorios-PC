75aa23c13014dbe158116816c354360dfbed7e02
javierlopez19 <humberthojavier@gmail.com>
2026-05-05 21:39:36 -0600

    Create Ejercicio 2.cs

---
+++ Laboratorio 13/Ejercicio 2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices.Java;
+class Vehiculo
+{
+    public string marca;
+    public string modelo;
+    public int anio;
+    public string color;
+    public string placa;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehiculo carro = new Vehiculo();
+
+        carro.marca = "Hyundai";
+        carro.modelo = "Accent";
+        carro.anio = 2001;
+        carro.color = "Corinto";
+        carro.placa = "P457JHL";
+
+        Console.WriteLine("Marca:" + carro.marca);
+        Console.WriteLine("Modelo:" + carro.modelo);
+        Console.WriteLine("Año:" + carro.anio);
+        Console.WriteLine("color:" + carro.color);
+        Console.WriteLine("Placa:" + carro.placa);
+
+    }
+
+
+}
