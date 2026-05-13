ef2a27795b0d38e8efda7a1124bb91be40debd61
javierlopez19 <humberthojavier@gmail.com>
2026-05-12 20:25:44 -0600

    Create ejercicio 2.cs

---
+++ Laboratorio 14/ejercicio 2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+public class Mascota
+{
+    private string nombre;
+    private string especie;
+    private int edad;
+    private bool vacunado;
+
+    public Mascota(string nombre, string especie, int edad, bool vacunado)
+    {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+        this.vacunado = vacunado;
+    }
+    public void MostrarInfo()
+    {
+        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Especie: " + especie);
+        Console.WriteLine("Edad: " + edad + "años");
+        Console.WriteLine("Vacunado: " + (vacunado ? "Sí" : "No"));
+        Console.WriteLine("");
+
+    }
+    public void Vacunar()
+    {
+        if (!vacunado)
+        {
+            vacunado = true;
+            Console.WriteLine(nombre + " ha sido vacunado.");
+        }
+        else
+        {
+            Console.WriteLine(nombre + " ya está vacunado.");
+        }
+    }
+    public void CumplirAnios()
+    {
+
+        edad++;
+        Console.WriteLine("La mascota " + nombre + " ahora tiene " + edad + " años.");
+
+    }
+
+
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Mascota mascota1 = new Mascota("Coco", "Perro", 7, true);
+        Mascota mascota2 = new Mascota("Perla", "Gato", 6, false);
+
+        Console.WriteLine("Información Inicial de las Mascotas: ");
+        mascota1.MostrarInfo();
+        mascota2.MostrarInfo();
+        Console.WriteLine("");
+
+        Console.WriteLine("Vamos a vacunar a Perla...");
+        mascota2.Vacunar();
+        mascota2.MostrarInfo();
+        Console.WriteLine("");
+
+        Console.WriteLine("Coco cumplió años...");
+        mascota1.CumplirAnios();
+        mascota1.MostrarInfo();
+        Console.WriteLine("");
+    }
+
+}
